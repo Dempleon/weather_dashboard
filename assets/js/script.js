79b508333bda2c7d19f664996e1267a15ac18c13
Dempoleon <dempoleon@gmail.com>
2022-10-10 20:48:48 -0700

    get daily average for humidity and windspeed, min and max temps

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,7 @@ async function get5days(lat, lon) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
+                    console.log(data.list);
                     displayForecast(data.list)
                 });
             } else {
@@ -89,13 +90,44 @@ function displayCurrent(data) {
 }
 
 function displayForecast(data) {
-    var days = [][];
     console.log(data);
-    for(var i = 0; i < data.length; i++) {
-        for(var j = 0; j < 8; j++) {
-            days.push()
-        }
+    var days = [];
+    //split the 40 data array into 5x8
+    while(data.length) {
+        days.push(data.splice(0,8));
     }
+    console.log(days);
+    for (var i = 0; i < days.length; i++) {
+        // getDailyMinMaxAvg(days[i]);
+        console.log(getDailyMinMaxAvg(days[i]));
+    }
+
 }
+
+function getDailyMinMaxAvg (day) {
+    console.log(day)
+    var minTemps = [];
+    var maxTemps = [];
+    var windspeeds = [];
+    var humidities = [];
+    for (var i = 0; i < day.length; i++) {
+        minTemps[i] = day[i].main.temp_min;
+        maxTemps[i] = day[i].main.temp_max;
+        windspeeds[i] = day[i].wind.speed;
+        humidities[i] = day[i].main.humidity;
+    }
+    return [Math.min(...minTemps), Math.max(...maxTemps), getAvg(windspeeds), getAvg(humidities)];
+    
+}
+
+function getAvg(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+
 
 
